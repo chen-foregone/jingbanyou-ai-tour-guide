7756f921ff4b47774a405cec20f751b80ed4d361
chen-foregone <13232397520@163.com>
2026-04-02 12:39:25 +0800

    init: 景伴游项目初始化 基于RuoYi-Vue二次开发

---
+++ jingbanyou-admin/src/main/java/cn/edu/gdou/jingbanyou/web/controller/system/SysRegisterController.java
@@ -0,0 +1,38 @@
+package cn.edu.gdou.jingbanyou.web.controller.system;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import cn.edu.gdou.jingbanyou.common.core.controller.BaseController;
+import cn.edu.gdou.jingbanyou.common.core.domain.AjaxResult;
+import cn.edu.gdou.jingbanyou.common.core.domain.model.RegisterBody;
+import cn.edu.gdou.jingbanyou.common.utils.StringUtils;
+import cn.edu.gdou.jingbanyou.framework.web.service.SysRegisterService;
+import cn.edu.gdou.jingbanyou.system.service.ISysConfigService;
+
+/**
+ * 注册验证
+ * 
+ * @author ruoyi
+ */
+@RestController
+public class SysRegisterController extends BaseController
+{
+    @Autowired
+    private SysRegisterService registerService;
+
+    @Autowired
+    private ISysConfigService configService;
+
+    @PostMapping("/register")
+    public AjaxResult register(@RequestBody RegisterBody user)
+    {
+        if (!("true".equals(configService.selectConfigByKey("sys.account.registerUser"))))
+        {
+            return error("当前系统没有开启注册功能！");
+        }
+        String msg = registerService.register(user);
+        return StringUtils.isEmpty(msg) ? success() : error(msg);
+    }
+}
